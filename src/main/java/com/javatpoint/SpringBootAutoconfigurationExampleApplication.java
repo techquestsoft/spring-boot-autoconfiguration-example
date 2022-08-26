3b5da2660905255acf0ecd553b50fcb141b2ded2
yekkuluri <rajasekharareddy@gmail.com>
2022-08-26 19:33:36 +0530

    adding project

---
+++ src/main/java/com/javatpoint/SpringBootAutoconfigurationExampleApplication.java
@@ -0,0 +1,11 @@
+package com.javatpoint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootAutoconfigurationExampleApplication 
+{
+public static void main(String[] args) 
+{
+SpringApplication.run(com.javatpoint.SpringBootAutoconfigurationExampleApplication .class, args);
+}
+}
